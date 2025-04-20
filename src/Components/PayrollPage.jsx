00b9d0e4335ac9e945596bd94d36722b89e02a33
Manigandan13 <138948807+Manigandan13@users.noreply.github.com>
2025-04-20 17:36:38 +0530

    Update PayrollPage.jsx

--- src/Components/PayrollPage.jsx
+++ src/Components/PayrollPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import "./PayrollPage.css"
+import api from '../api/api'
 
 function PayrollPage() {
   const [employeeId, setEmployeeId] = useState('');
